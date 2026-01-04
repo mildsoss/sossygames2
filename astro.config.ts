7954799f6deee0f91ea315a0de08c22faafc5554
Mildsoss <mildsoss@Mildsoss.local>
2026-01-04 13:30:41 -0600

    add tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,11 @@
 import solidJs from "@astrojs/solid-js";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [solidJs()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
